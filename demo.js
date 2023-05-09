1f79beddc04d23d98f681ab63b041696a9ca787d
jcatekista <jcatekista@gmail.com>
2023-05-08 17:26:35 -0700

    added workshop js file

---
+++ demo.js
@@ -0,0 +1,29 @@
+//vending machine tests
+
+//unit
+//card reader
+//when card swioed, check for money, machine goes ready for selection
+
+//cash insert
+
+//coin return
+
+//button selection
+//when pressing "a1" screen reads out "a1"
+//vending slot
+
+//hvac
+
+
+
+//intergration
+
+
+
+
+//functional
+
+
+
+
+//acceptance
