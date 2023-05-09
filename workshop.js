099711afc7f20461ff61d944d71a96326e7fdaea
jcatekista <jcatekista@gmail.com>
2023-05-08 17:40:29 -0700

    no codes just notes for workshop

---
+++ workshop.js
@@ -0,0 +1,24 @@
+//multiplication function
+
+//Expect multiplication (2,3) to be a number
+//Expect multiplication (2,3) to be equal to 6
+//Expect multiplication (a,3) to be an error
+//Expect multiplication (n1, n2) inputs to be numbers
+//Expect multiplication (n1, n2) to error if input is anything but a number
+
+//concatOdds function
+//Expect concatOdds([3,2,1]), [9,1,1,1,4,15,-1]) to be digits
+//Expect concatOdds([3,2,1]), [9,1,1,1,4,15,-1]) if any input is not a digit return error
+//Expect concatOdds([3,2,1]), [9,1,1,1,4,15,-1]) should not be booleans, or strings, if so , return error
+//Expect concatOdds([3,2,1]), [9,1,1,1,4,15,-1]) to return odd numbers only
+//Expect concatOdds([3,2,1]), [9,1,1,1,4,15,-1]) should not return duplicate numbers
+//Expect concatOdds([3,2,1]), [9,1,1,1,4,15,-1]) to return [-1,1,3,9,15]
+
+//functional test
+//Expect checkout[submit button] to return sign-in prompt
+//Expect checkout[submit button] sign-in option as guest
+//Expect checkout [submit button] sign-in option as account
+//Expect checkout[submit button] to have items in cart
+//Expect checkout[submit button] empty to return "cart is empty" prompt
+//Expect checkout[submit button] to move to address confirmation after sign-in
+//
